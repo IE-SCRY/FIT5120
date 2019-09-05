e640e66447f9ece6472c92db327b6389c80ae2a3
ygee0006 <43592321+ygee0006@users.noreply.github.com>
2019-09-05 19:57:32 +1000

    Legal Genie V1

---
+++ HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Knowledgebase()
+        {
+            ViewBag.Message = "Knowledgebase";
+
+            return View();
+        }
+
+        public ActionResult RentComparison()
+        {
+            ViewBag.Message = "Rent Comparison";
+
+            return View();
+        }
+
+        public ActionResult LegalGames()
+        {
+            ViewBag.Message = "Legal Games";
+
+            return View();
+        }
+
+        public ActionResult CompensationForDamages()
+        {
+            ViewBag.Message = "Compensation For Damages";
+
+            return View();
+        }
+
+        public ActionResult Questionaire()
+        {
+            ViewBag.Message = "Questionaire";
+
+            return View();
+        }
+
+    }
+}
