6e919437caef68bfd61bfab37b2df2935fa73fb1
ygee0006 <43592321+ygee0006@users.noreply.github.com>
2019-09-16 18:21:06 +1000

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IEV3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+
+        public ActionResult Knowledgebase()
+        {
+            return View();
+        }
+
+        public ActionResult Questionnaire()
+        {
+            return View();
+        }
+        public ActionResult RentComparison()
+        {
+            return View();
+        }
+    }
+}
